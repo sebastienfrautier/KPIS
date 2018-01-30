38bcfa096216b13a6cbab4d38600e67598b9157f
Sidney Hartmann <4shartma@informatik.uni-hamburg.de>
2018-01-30 11:48:57 +0100

    Added option to save and continue training

--- me_pong.py
+++ me_pong.py
@@ -33,17 +33,16 @@
 
 import gym
 import numpy as np
+import pickle
+import datetime as date
+import os
+import argparse
 
 
 def downsample(image):
     # Take only alternate pixels - basically halves the resolution of the image (which is fine for us)
-    #print np.shape(image)
     return image[::2, ::2, :]
 
-def softmax(x):
-    """Compute softmax values for each sets of scores in x."""
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum()
 
 def remove_color(image):
     """Convert all color (RGB is the third dimension in the image)"""
@@ -80,7 +79,6 @@ def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
 
 
-
 def relu(vector):
     vector[vector < 0] = 0
     return vector
@@ -91,39 +89,26 @@ def apply_neural_nets(observation_matrix, weights):
     hidden_layer_values = np.dot(weights['1'], observation_matrix)
     hidden_layer_values = relu(hidden_layer_values)
     output_layer_values = np.dot(hidden_layer_values, weights['2'])
-    output_layer_values = softmax(output_layer_values)
-    #print hidden_layer_values
-    print output_layer_values
+    output_layer_values = sigmoid(output_layer_values)
     return hidden_layer_values, output_layer_values
 
 
 def choose_action(probability):
     random_value = np.random.uniform()
-    #if random_value < probability:
-    #    # signifies up in openai gym
-    #    return 2
-    #else:
-    #    # signifies down in openai gym
-    #    return 3
-    if (probability < random_value).any():
-        return probability.argmax()
+    if random_value < probability:
+        # signifies up in openai gym
+        return 2
     else:
-        return np.random.randint(1,5)
+        # signifies down in openai gym
+        return 3
+
 
 def compute_gradient(gradient_log_p, hidden_layer_values, observation_values, weights):
     """ See here: http://neuralnetworksanddeeplearning.com/chap2.html"""
-    print "compute_gradient call"
-
     delta_L = gradient_log_p
     dC_dw2 = np.dot(hidden_layer_values.T, delta_L).ravel()
     delta_l2 = np.outer(delta_L, weights['2'])
     delta_l2 = relu(delta_l2)
-
-    print 'gradient_log_p.shape %s.' % (gradient_log_p.shape,)
-    print 'delta_l2.T.shape %s.' % (delta_l2.T.shape ,)
-    print 'observation_values.shape %s.' % (observation_values.shape, )
-    print 'weights["1"] %s.' % (weights['1'].shape, )
-    print 'weights["2"] %s.' % (weights['2'].shape, )
     dC_dw1 = np.dot(delta_l2.T, observation_values)
     return {
         '1': dC_dw1,
@@ -164,13 +149,29 @@ def discount_with_rewards(gradient_log_p, episode_rewards, gamma):
 
 
 def main():
-    #env = gym.make("Pong-v0")
-    #env = gym.make("CartPole-v1")
-    #env = gym.make("Pitfall-v0")
-    #env = gym.make("Pooyan-v0")
-    #env = gym.make("AirRaid-v0")
-    env = gym.make("MsPacman-v0")
+    env = gym.make("Pong-v0")
     observation = env.reset()  # This gets us the image
+    #setup
+    parser = argparse.ArgumentParser(description='Pong')
+
+    parser.add_argument('-e', help='number of episodes')
+
+    parser.add_argument('-r', help='run with trained weights')
+
+    parser.add_argument('-w', help='write last weights')
+
+    parser.add_argument('-mode', help='train or run')
+
+    parser.add_argument('-c', help='continue with config file')
+
+    args = parser.parse_args()
+
+    if args.e:
+        print "setting max episode to "+args.e
+        max_episode = args.e
+    else:
+        max_episode = 100
+
 
     # hyperparameters
     episode_number = 0
@@ -183,12 +184,13 @@ def main():
 
     episode_number = 0
     reward_sum = 0
+    running_reward_list=[]
     running_reward = None
     prev_processed_observations = None
 
     weights = {
         '1': np.random.randn(num_hidden_layer_neurons, input_dimensions) / np.sqrt(input_dimensions),
-        '2': np.random.randn(num_hidden_layer_neurons,4) / np.sqrt(num_hidden_layer_neurons)
+        '2': np.random.randn(num_hidden_layer_neurons) / np.sqrt(num_hidden_layer_neurons)
     }
 
     # To be used with rmsprop algorithm (http://sebastianruder.com/optimizing-gradient-descent/index.html#rmsprop)
@@ -200,7 +202,102 @@ def main():
 
     episode_hidden_layer_values, episode_observations, episode_gradient_log_ps, episode_rewards = [], [], [], []
 
+
+    if args.w == 'true':
+        write = 'true'
+    else:
+        write = None
+
+    if args.mode == 'train':
+        if args.c:
+            print 'Entering training with config file: '+args.c
+            absolute_weights_path = os.path.dirname(os.path.abspath(__file__)) + '/weights/'
+            with open(absolute_weights_path + args.c, 'rb') as handle:
+                config = pickle.load(handle)
+
+            weights =config['weights']
+            running_reward=config['running_reward']
+            episode_number = config['episode_number']
+            running_reward_list=config['running_reward_list']
+
+            gamma=config['hyperparameter']['gamma']
+            learning_rate= config['hyperparameter']['learning_rate']
+            decay_rate=config['hyperparameter']['decay_rate']
+            batch_size= config['hyperparameter']['batch_size']
+
+            print 'Entering training with reward_sum: ' + str(running_reward)
+
+        print 'Entering training mode train'
+        train(env
+              ,episode_number
+              ,max_episode
+              ,input_dimensions
+              ,weights
+              ,0
+              ,g_dict
+              ,batch_size
+              ,expectation_g_squared
+              ,decay_rate
+              ,learning_rate
+              ,gamma
+              ,observation
+              ,prev_processed_observations
+              ,[]
+              ,[]
+              ,[]
+              ,[]
+              ,running_reward=running_reward
+              ,running_reward_list=running_reward_list)
+
+    if args.mode == 'run':
+        run(args.c,env,observation, prev_processed_observations,input_dimensions)
+
+
+def run(file_name,env,observation, prev_processed_observations,input_dimensions):
+    absolute_weights_path = os.path.dirname(os.path.abspath(__file__)) + '/weights/'
+
+    with open(absolute_weights_path+file_name, 'rb') as handle:
+        config = pickle.load(handle)
     while True:
+        env.render()
+        processed_observations, prev_processed_observations = preprocess_observations(observation,
+                                                                                      prev_processed_observations,
+                                                                                      input_dimensions)
+        hidden_layer_values, up_probability = apply_neural_nets(processed_observations, config['weights'])
+
+        action = choose_action(up_probability)
+
+        # carry out the chosen action
+        observation, reward, done, info = env.step(action)
+        if done:
+            env.reset()  # reset env
+
+
+
+def train(env
+          ,episode_number
+          , max_episode
+          , input_dimensions
+          , weights
+          , reward_sum
+          , g_dict
+          , batch_size
+          , expectation_g_squared
+          , decay_rate
+          , learning_rate
+          , gamma
+          , observation
+          , prev_processed_observations
+          , episode_hidden_layer_values
+          , episode_observations, episode_gradient_log_ps, episode_rewards
+            ,running_reward_list
+          ,running_reward =None
+
+          ):
+
+    end_episode=episode_number+int(max_episode)
+    print('new end episode %f with episode_number %f and max %s' % (end_episode, episode_number, max_episode))
+    while episode_number < end_episode:
         env.render()
         processed_observations, prev_processed_observations = preprocess_observations(observation,
                                                                                       prev_processed_observations,
@@ -225,7 +322,7 @@ def main():
 
         if done:  # an episode finished
             episode_number += 1
-            print "EPISODE DONE"
+            print("Current episode: ")+str(episode_number)
             # Combine the following values for the episode
             episode_hidden_layer_values = np.vstack(episode_hidden_layer_values)
             episode_observations = np.vstack(episode_observations)
@@ -252,9 +349,36 @@ def main():
             episode_hidden_layer_values, episode_observations, episode_gradient_log_ps, episode_rewards = [], [], [], []  # reset values
             observation = env.reset()  # reset env
             running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
-            print 'resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward)
+            running_reward_list.append(running_reward)
+            print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
             reward_sum = 0
             prev_processed_observations = None
+
+    #path = '../weights/'
+    if episode_number == end_episode:
+        file_name = 'Pong_'+ date.datetime.now().isoformat()+'_episode_'+str(episode_number)
+
+        config = {'weights': weights
+                ,'episode_number': episode_number
+                ,'reward_sum' : reward_sum
+                , 'running_reward' : running_reward
+                , 'running_reward_list' : running_reward_list
+                , 'hyperparameter' : {'gamma':gamma
+                                        ,'batch_size':batch_size
+                                        , 'learning_rate': learning_rate
+                                        , 'decay_rate': decay_rate}
+                  }
+
+
+        absolute_weights_path = os.path.dirname(os.path.abspath(__file__))+'/weights/'
+
+
+        print absolute_weights_path+file_name
+
+        with open(absolute_weights_path+file_name, 'wb') as handle:
+                pickle.dump(config, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
 
 
 main()
